4e777a7ed1165bcf731057953b8b00dbbbc4db5c
yuantiankai <284989065@qq.com>
2019-10-09 16:23:05 +0800

    add crontab

--- test_webssh/start.sh
+++ test_webssh/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-&& /etc/init.d/cron start \
-&& python /app/manage.py crontab add
+/etc/init.d/cron start
+python /app/manage.py crontab add
 python /app/manage.py collectstatic --noinput
 python /app/manage.py makemigrations app01 --noinput
 python /app/manage.py migrate app01 --fake-initial --noinput
